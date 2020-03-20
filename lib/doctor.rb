fe7c6549840f6c75765f1f0d5613f5f7330f578d
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-20 15:38:23 +0000

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -20,8 +20,8 @@ class Doctor
     @@all
   end
 
-  def new_appointment(date,patient)
-    Appointment.new(date,patient,self)
+  def new_appointment(patient,date)
+    Appointment.new(date, patient, self)
   end
 
 end
